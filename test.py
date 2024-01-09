2bc34bd1f8f786d64aae3ed70bb018970a344913
Kashif Ali Bhutto <136756981+Kashif3216@users.noreply.github.com>
2024-01-09 13:17:33 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.write('hello worls')
+def addition_calculator():
+    st.title("ADDITION Calculator")
+
+    x = st.number_input("Enter first number:")
+    y = st.number_input("Enter second number:")
+
+    add_button = st.button("Add Numbers")
+
+    if add_button:
+        sum_result = x + y
+        st.success(f"Sum of your numbers is: {sum_result}")
+
+if __name__ == "__main__":
+    addition_calculator()
+
+
+
